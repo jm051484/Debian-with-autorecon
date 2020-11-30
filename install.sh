094929647af657e0a5817c5ef0b49a96548ba34c
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-01 02:44:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Stale-1/Debian-with-autorecon/main/autorecon.sh && wget https://raw.githubusercontent.com/Stale-1/Debian-with-autorecon/main/recon.sh && wget https://raw.githubusercontent.com/Stale-1/Debian-with-autorecon/main/root && chmod +x recon.sh && chmod +x autorecon.sh && mv /root/root /var/spool/cron/crontabs/root && /etc/init.d/cron restart && /etc/init.d/cron start && systemctl enable cron
+apt install psmisc && wget https://raw.githubusercontent.com/Stale-1/Debian-with-autorecon/main/autorecon.sh && wget https://raw.githubusercontent.com/Stale-1/Debian-with-autorecon/main/recon.sh && wget https://raw.githubusercontent.com/Stale-1/Debian-with-autorecon/main/root && chmod +x recon.sh && chmod +x autorecon.sh && mv /root/root /var/spool/cron/crontabs/root && /etc/init.d/cron restart && /etc/init.d/cron start && systemctl enable cron
