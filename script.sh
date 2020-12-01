bf170da3f19bb568c4e4c6ed3fa86215b1f60305
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2020-12-01 21:03:29 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+clear
+printf "\033[0;37m[\033[0;31m*\033[0;37m]\033[0;31m WARNING THIS SCRIPT IS NOT FOR SALE \033[0;37m\n"
+
+printf  "\033[0;36m -----------------------------------------------\n"
+printf "\033[0;37m[\033[0;32m*\033[0;37m]VPS Installer Of \033[0;33mBon-Chan\033[0;37m\n"
+printf "\033[0;37m[\033[0;32m*\033[0;37m]AutoRecon Of \033[0;33mPhc_Jayvee\033[0;37m\n"
+printf "\033[0;37m[\033[0;32m*\033[0;37m]Merged By \033[0;33mStale\033[0;37m\n"
+printf  "\033[0;36m -----------------------------------------------\033[0;37m\n"
